01d2a71ae698eb5b72984f023843aafc821b5365
nickyqqq <nagornnicky@gmail.com>
2025-10-05 01:35:00 +0700

    publishing

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig(() => ({
-  base: '/exoplanet1/',
+  base: '/exoplanet-front-end-design/',
   server: {
     host: "::",
     port: 8080,
